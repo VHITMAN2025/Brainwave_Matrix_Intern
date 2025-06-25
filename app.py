2eb427f349d5deb953cbea4087023b8624ca3243
HITMAN <101077891+VHITMAN2025@users.noreply.github.com>
2025-06-25 23:46:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import pickle
 import re
 from nltk.corpus import stopwords
+import os
 
 # Load model and vectorizer
 with open('model.pkl', 'rb') as f:
@@ -27,4 +28,5 @@ def index():
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
